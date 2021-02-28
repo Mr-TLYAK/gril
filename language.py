a925105c71d328db28607593e7347ee870b8433a
Mr-TLYAK <79798069+Mr-TLYAK@users.noreply.github.com>
2021-02-28 20:10:22 +0300

    Add files via upload

---
+++ language.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup as parser
+
+def main(cookie, url, config):
+	try:
+		response = config.httpRequest(url+'/language.php', cookie).encode('utf-8')
+		html = parser(response, 'html.parser')
+		for x in html.find_all('a'):
+			if 'Bahasa Indonesia' in str(x):
+				config.httpRequest(url+x['href'], cookie)
+				break
+	except: pass
